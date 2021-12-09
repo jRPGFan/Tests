01de831f6906a892e11fc023d3e2496f990c2fd9
Stora_Pavel <jRPGNyan@gmail.com>
2021-12-09 18:56:42 +0200

    HW_3

--- app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
+++ app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
@@ -108,4 +108,10 @@ class SearchPresenterTest {
         verify(viewContract, times(1))
             .displaySearchResults(searchResults, 101)
     }
+
+    @Test
+    fun onDetach() {
+        presenter.onDetach()
+        assertNull(presenter.viewContract)
+    }
 }
